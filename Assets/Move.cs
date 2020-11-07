1001eca9d7a2bb768c4fb9400250471f5662c0a2
urouault <urouault@amplitude-studios.com>
2020-11-07 15:11:28 +0100

    Baisse toi et vole

---
+++ Assets/Move.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    // get center camera (audio listener because reason)
+    AudioListener MainCamera;
+
+    public float speedCoef = 1f;
+
+    public float maxHeight = 2f;
+    public float minHeight = 1f;
+
+
+
+    public float height = 0;
+
+    public float DebugActualSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake(){
+        MainCamera = GetComponentInChildren<AudioListener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      /*  RaycastHit hit;
+        Physics.Raycast(MainCamera.transform.position, Vector3.down, out hit, 100);
+
+        if(hit.distance > 0){
+            height = hit.distance;
+        }*/
+
+    height = MainCamera.transform.localPosition.y;
+
+    if(height > maxHeight) height = maxHeight;
+    if(height < minHeight) height = minHeight;
+
+    float coef = Mathf.InverseLerp(maxHeight, minHeight, height);
+
+
+    transform.Translate(MainCamera.transform.forward * coef * speedCoef);
+
+    }
+}
