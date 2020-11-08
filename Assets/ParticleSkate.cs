96b7aedb6545d1798e32a2ba3d86db3493dc4f2e
Giome <guillaume.dartoen@gmail.com>
2020-11-08 16:18:53 +0100

    TurboVFX properly integrated

--- Assets/ParticleSkate.cs
+++ Assets/ParticleSkate.cs
@@ -6,28 +6,40 @@ public class ParticleSkate : MonoBehaviour
 {
     public ParticleSystem PSVitesse;
     public ParticleSystem PSTurbo;
+    public ParticleSystem PSTurbo2;
     private float ActualSpeed;
     // Start is called before the first frame update
     void Start()
     {
         ActualSpeed = transform.parent.parent.GetComponent<Move>().actualSpeed;
         PSVitesse.gameObject.SetActive(false);
+        PSTurbo.gameObject.SetActive(false);
+        PSTurbo2.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         var EmissionPSV = PSVitesse.emission;
+        var EmissionPST1 = PSTurbo.emission;
+        var EmissionPST2 = PSTurbo2.emission;
+
         ActualSpeed = transform.parent.parent.GetComponent<Move>().actualSpeed;
-        Debug.Log(ActualSpeed);
+
         if(ActualSpeed > 0.1f)
         {
             PSVitesse.gameObject.SetActive(true);
+            PSTurbo.gameObject.SetActive(true);
+            PSTurbo2.gameObject.SetActive(true);
             EmissionPSV.rateOverTime = ActualSpeed * 500;
+            EmissionPST1.rateOverTime = ActualSpeed * 100;
+            EmissionPST2.rateOverTime = ActualSpeed * 100;
         }
         else
         {
             PSVitesse.gameObject.SetActive(false);
+            PSTurbo.gameObject.SetActive(false);
+            PSTurbo2.gameObject.SetActive(false);
         }  
     }
 }
