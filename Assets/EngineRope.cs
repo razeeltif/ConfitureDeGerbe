1af14e0ff26834784f04c7f55c78fb8a77c1fc81
Giome <guillaume.dartoen@gmail.com>
2020-11-08 22:30:50 +0100

    Nice Rope for handle

---
+++ Assets/EngineRope.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineRope : MonoBehaviour
+{
+
+    LineRenderer Rope;
+    public GameObject LineEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rope = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rope.SetPosition(0, gameObject.transform.position);
+        Rope.SetPosition(1, LineEnd.transform.position);
+
+    }
+}
