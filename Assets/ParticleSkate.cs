b6bccad42558fc7d4cd761ad9372f3e64b5d907e
Giome <guillaume.dartoen@gmail.com>
2020-11-08 23:10:41 +0100

    Wheel animation

--- Assets/ParticleSkate.cs
+++ Assets/ParticleSkate.cs
@@ -7,7 +7,9 @@ public class ParticleSkate : MonoBehaviour
     public ParticleSystem PSVitesse;
     public ParticleSystem PSTurbo;
     public ParticleSystem PSTurbo2;
+    Animator animator;
     private float ActualSpeed;
+    public bool stopped;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +17,13 @@ public class ParticleSkate : MonoBehaviour
         PSVitesse.gameObject.SetActive(false);
         PSTurbo.gameObject.SetActive(false);
         PSTurbo2.gameObject.SetActive(false);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Stopped", stopped);
         var EmissionPSV = PSVitesse.emission;
         var EmissionPST1 = PSTurbo.emission;
         var EmissionPST2 = PSTurbo2.emission;
@@ -28,6 +32,7 @@ public class ParticleSkate : MonoBehaviour
 
         if(ActualSpeed > 0.1f)
         {
+            stopped = false;
             PSVitesse.gameObject.SetActive(true);
             PSTurbo.gameObject.SetActive(true);
             PSTurbo2.gameObject.SetActive(true);
@@ -37,6 +42,7 @@ public class ParticleSkate : MonoBehaviour
         }
         else
         {
+            stopped = true;
             PSVitesse.gameObject.SetActive(false);
             PSTurbo.gameObject.SetActive(false);
             PSTurbo2.gameObject.SetActive(false);
